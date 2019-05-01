d0fb6262e4c8d8df2970f3fc52976dd447d8c917
aurélien <aurelienmay98@gmail.com>
2019-05-01 18:27:01 +0200

    test add comment

--- src/Main/SmartphoneON.java
+++ src/Main/SmartphoneON.java
@@ -8,6 +8,7 @@ public class SmartphoneON {
     public static void main(String []args){
         Frame frame = new Frame();
         frame.setSize(323,645);
+        //200 sur le pc fixe
         frame.setLocation(800,100);
         frame.setVisible(true);
     }
